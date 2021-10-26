cd4b8fb4bf1be94d4396f3d3b1c8e4c6f9547a25
unknown <hoaela121@gmail.com>
2021-10-27 03:46:49 +0800

    Activity Question Basic

--- app/src/main/java/com/example/capstone/Activity/QuestionActivity.java
+++ app/src/main/java/com/example/capstone/Activity/QuestionActivity.java
@@ -1,8 +1,15 @@
 package com.example.capstone.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,7 +23,8 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
     private TextView tvQuizChapterNumber,tvQuizChapterTitle,tvType,tvQuestion,tvTimer,tvCount;
     private Button btnA,btnB,btnC,btnD;
     private List<Question> mQuestionList;
-
+    private int questionNumber;
+    private CountDownTimer countdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +40,10 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         tvCount=findViewById(R.id.tvCount);
 
         //buttons
-        btnA=findViewById(R.id.btnA);
-        btnB=findViewById(R.id.btnB);
-        btnC=findViewById(R.id.btnC);
-        btnD=findViewById(R.id.btnD);
+        btnA=(Button)findViewById(R.id.btnA);
+        btnB=(Button)findViewById(R.id.btnB);
+        btnC=(Button)findViewById(R.id.btnC);
+        btnD=(Button)findViewById(R.id.btnD);
 
         btnA.setOnClickListener(this);
         btnB.setOnClickListener(this);
@@ -50,13 +58,170 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         mQuestionList=new ArrayList();
         mQuestionList.add(new Question("Question 1","Option A", "Option B", "Option C", "Option D",2));
         mQuestionList.add(new Question("Question 2","Option B", "Option B", "Option C", "Option D",2));
-        mQuestionList.add(new Question("Question 3","Option ", "Option B", "Option C", "Option D",2));
-        mQuestionList.add(new Question("Question 4","Option D", "Option B", "Option C", "Option D",2));
+        mQuestionList.add(new Question("Question 3","Option C", "Option D", "Option C", "Option B",2));
+        mQuestionList.add(new Question("Question 4","Option D", "Option D", "Option C", "Option B",2));
+
+        setQuestion();
+
+    }
+
+    private void setQuestion() {
+        tvTimer.setText(String.valueOf(10));
+        tvQuestion.setText(mQuestionList.get(0).getQuestion());
+        btnA.setText(mQuestionList.get(0).getOptionA());
+        btnB.setText(mQuestionList.get(0).getOptionB());
+        btnC.setText(mQuestionList.get(0).getOptionC());
+        btnD.setText(mQuestionList.get(0).getOptionD());
+
+        tvCount.setText(String.valueOf(1)+"/"+String.valueOf(mQuestionList.size()));
+    
+        startTimer();
+
+        questionNumber=0;
+    }
+
+    private void startTimer() {
+        countdown=new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tvTimer.setText(String.valueOf(millisUntilFinished/1000));
+
+            }
+            @Override
+            public void onFinish() {
+
+                changeQuestion();
+            }
+        };
+
+        countdown.start();
 
     }
 
     @Override
     public void onClick(View v) {
+        int selectedButton=0;
+
+        switch(v.getId()){
+            case R.id.btnA:
+                selectedButton=1;
+                break;
+            case R.id.btnB:
+                selectedButton=2;
+                break;
+            case R.id.btnC:
+                selectedButton=3;
+                break;
+            case R.id.btnD:
+                selectedButton=4;
+                break;
+
+            default:
+        }
+        countdown.cancel();
+        checkAnswer(selectedButton,v);
+
+    }
+
+    private void checkAnswer(int selectedButton, View view) {
+        if(selectedButton==mQuestionList.get(questionNumber).getCorrectAnswer()){
+            //answer
+            ((Button)view).setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+
+        }else{
+            ((Button)view).setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+
+            switch(mQuestionList.get(questionNumber).getCorrectAnswer()){
+                case 1:
+                    btnA.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    break;
+                case 2:
+                    btnB.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    break;
+                case 4:
+                    btnC.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    break;
+                case 5:
+                    btnD.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    break;
+
+            }
+        }
+        changeQuestion();
+
+    }
+
+    private void changeQuestion() {
+        if (questionNumber<mQuestionList.size()-1){
+            //play animation to change the question
+
+            questionNumber++;
+
+            playAnim(tvQuestion,0,0);
+            playAnim(btnA,0,1);
+            playAnim(btnB,0,2);
+            playAnim(btnC,0,3);
+            playAnim(btnD,0,4);
+
+            tvCount.setText(String.valueOf(questionNumber+1)+"/"+String.valueOf(mQuestionList.size()));
+
+            tvTimer.setText(String.valueOf(10));
+            startTimer();
+
+
+        }else{
+            //go to score act
+             Intent i= new Intent(QuestionActivity.this,ScoreActivity.class);
+             startActivity(i);
+             QuestionActivity.this.finish();
+        }
+
+    }
+
+    private void playAnim(View view, final int value,int viewNumber) {
+        view.animate().alpha(value).scaleX(value).scaleY(value).setDuration(500)
+                .setStartDelay(100).setInterpolator(new DecelerateInterpolator())
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        if(value==0){
+                            switch(viewNumber){
+                                case 0:
+                                    ((TextView)view).setText(mQuestionList.get(questionNumber).getQuestion());
+                                    break;
+                                case 1:
+                                    ((Button)view).setText(mQuestionList.get(questionNumber).getOptionA());
+                                    break;
+                                case 2:
+                                    ((Button)view).setText(mQuestionList.get(questionNumber).getOptionB());
+                                    break;
+                                case 3:
+                                    ((Button)view).setText(mQuestionList.get(questionNumber).getOptionC());
+                                    break;
+                                case 4:
+                                    ((Button)view).setText(mQuestionList.get(questionNumber).getOptionD());
+                                    break;
+                            }
+                            playAnim(view,1,viewNumber);
+                        }
+
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
 
     }
 }
