8b0e4c0ffdc12bb1b5c38c051690636f6ac3d9c5
unknown <hoaela121@gmail.com>
2021-11-16 12:01:47 +0800

    TestFragment

--- app/src/main/java/com/example/capstone/Fragment/TestFragment.java
+++ app/src/main/java/com/example/capstone/Fragment/TestFragment.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -94,4 +96,5 @@ public class TestFragment extends Fragment {
 
         return rootView;
     }
+
 }
