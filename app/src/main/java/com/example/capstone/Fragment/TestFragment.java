a18adda329dac519b2c2009b4c942a97f4a0f506
unknown <hoaela121@gmail.com>
2021-12-31 19:11:32 +0800

    Title Change

--- app/src/main/java/com/example/capstone/Fragment/TestFragment.java
+++ app/src/main/java/com/example/capstone/Fragment/TestFragment.java
@@ -38,6 +38,8 @@ public class TestFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
